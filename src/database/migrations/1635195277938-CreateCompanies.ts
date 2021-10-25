afb1f44fff79d22aaa100421ea06d798e2f29171
Francisco Barros <francisco-barros@bitbucket.org>
2021-10-25 18:25:28 -0300

    add migrations to create users and companies table

---
+++ src/database/migrations/1635195277938-CreateCompanies.ts
@@ -0,0 +1,118 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateCompanies1635195277938 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'companies',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'phone',
+            type: 'varchar'
+          },
+          {
+            name: 'cep',
+            type: 'varchar'
+          },
+          {
+            name: 'address',
+            type: 'varchar'
+          },
+          {
+            name: 'number',
+            type: 'varchar'
+          },
+          {
+            name: 'complement',
+            type: 'varchar'
+          },
+          {
+            name: 'neighborhood',
+            type: 'varchar'
+          },
+          {
+            name: 'city',
+            type: 'varchar'
+          },
+          {
+            name: 'state',
+            type: 'varchar'
+          },
+          {
+            name: 'latitude',
+            type: 'varchar'
+          },
+          {
+            name: 'longitude',
+            type: 'varchar'
+          },
+          {
+            name: 'description',
+            type: 'varchar'
+          },
+          {
+            name: 'priority',
+            type: 'boolean',
+            default: false
+          },
+          {
+            name: 'site',
+            type: 'varchar'
+          },
+          {
+            name: 'facebook',
+            type: 'varchar'
+          },
+          {
+            name: 'instagram',
+            type: 'varchar'
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+            default: true
+          },
+          {
+            name: 'cpfCnpj',
+            type: 'varchar'
+          },
+          {
+            name: 'userId',
+            type: 'uuid'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ],
+        foreignKeys: [{
+          name: 'FKUserCompany',
+          referencedTableName: 'users',
+          referencedColumnNames: ['id'],
+          columnNames: ['userId'],
+          onUpdate: 'SET NULL',
+          onDelete: 'SET NULL'
+        }]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('companies')
+  }
+}
