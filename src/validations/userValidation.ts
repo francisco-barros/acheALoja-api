079a6d662017da7ed08a9ebfc5bfea7330a6b9d0
Francisco Barros <francisco-barros@bitbucket.org>
2021-10-29 11:56:58 -0300

    fix errors

--- src/validations/userValidation.ts
+++ src/validations/userValidation.ts
@@ -1,12 +1,11 @@
 import { celebrate, Joi, Segments } from 'celebrate'
-import Validate from 'validate-cpf-joi'
 
 export default celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    cpf: Validate.document().cpf().required(),
+    cpf: Joi.string().required(),
     phone: Joi.string().optional(),
     cellphone: Joi.string().optional(),
     admin: Joi.boolean().optional(),
