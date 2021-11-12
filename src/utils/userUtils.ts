05b02018572b4d9dcfbd41f5f6ee08f89e1d9d41
Francisco Barros <francisco-barros@bitbucket.org>
2021-11-12 17:54:57 -0300

    fix UserUtils

--- src/utils/userUtils.ts
+++ src/utils/userUtils.ts
@@ -1,7 +1,8 @@
 import { User } from '@/entities/User'
 import { UserRepository } from '@/repositories'
+import { getCustomRepository } from 'typeorm'
 
-const userRepository = new UserRepository()
+const userRepository = getCustomRepository(UserRepository)
 export const getUser = async (id: string): Promise<User | undefined> => {
   const user = await userRepository.findOne({ id })
 
